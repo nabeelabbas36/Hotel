86cd0449368f0a273bed7081202ab8afa6c01687
nabeel <nabeelabbas36@yahoo.com>
2013-10-08 16:25:08 +0500

    changes done after trimming double quotes from string and adding country select dropdown in locaions

---
+++ app/controllers/admins/locations_controller.rb
@@ -0,0 +1,79 @@
+class Admins::LocationsController < ApplicationController
+  layout 'admin'
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
+  # GET /locations
+  # GET /locations.json
+  def index
+   @locations = Location.all
+    @json = Location.all.to_gmaps4rails
+  end
+
+  # GET /locations/1
+  # GET /locations/1.json
+  def show
+  end
+
+  # GET /locations/new
+  def new
+    @location = Location.new
+ 
+  end
+
+  # GET /locations/1/edit
+  def edit
+  end
+
+  # POST /locations
+  # POST /locations.json
+  def create
+    @location = Location.new(secure_params)
+    
+    if @location.save
+      flash[:notice] = "Message sent from #{@location.name}."
+      redirect_to  admins_locations_path
+    else
+      render :new
+    end
+  end
+  private
+  def secure_params
+    params.require(:location).permit(:area, :address, :start_time,:close_time)
+  end
+  end
+
+  # PATCH/PUT /locations/1
+  # PATCH/PUT /locations/1.json
+  def update
+    respond_to do |format|
+      if @location.update(location_params)
+        format.html { redirect_to @location, notice: 'Location was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @location.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /locations/1
+  # DELETE /locations/1.json
+  def destroy
+    @location.destroy
+    respond_to do |format|
+      format.html { redirect_to locations_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def location_params
+      params.require(:location).permit(:name, :address, :longitude, :latitude, :gmaps, :start_time, :close_time)
+    end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.destroy_all
 (0..10).each do |a|
-  
   user = User.new
   user.email = "user#{a}@gmail.com"
   user.password = user.password_confirmation = "12345678"
@@ -20,64 +13,39 @@ admin.password = admin.password_confirmation = "12345678"
 admin.save
 
 
-# to seed database with countries 
-#execute "LOAD DATA INFILE '#{::Rails.root.join('db','migrate','GeoWorldMap', 'Countries.txt')} INTO TABLE countries FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;"
-# to seed database with states
-#execute "LOAD DATA INFILE '#{Rails.root}/db/migrate/GeoWorldMap/Regions.txt' INTO TABLE regions
-# FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;"
-# to seed database with cities
-#execute "LOAD DATA INFILE '#{RAILS_ROOT}/db/migrate/GeoWorldMap/Cities.txt' INTO TABLE cities
-# FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES;"
 Country.destroy_all
 File.open(Rails.root.join('db','migrate','GeoWorldMap', 'Countries.txt'), "r").each do |line|
-   country_from_file = line.split(",") 
-   Country.create! do |country|
-    country.id = country_from_file[0].to_i
-    country.name= country_from_file[1].to_s
+  country_from_file = line.split(",") 
+  unless (country_from_file[1].gsub /"/, '') == "Country"
+    Country.create! do |country|
+      country.id = country_from_file[0].to_i
+      country.name= country_from_file[1].gsub /"/, ''
     end
-end
-#  name, birthdate = line.strip.split("")
-#  user = User.new(user_name: name, birth_date: birthdate)
-#  user.save
-#end
-#require "csv"
-#
-#CSV.foreach(Rails.root.join("Countries1.csv"), headers: true) do |row|
-#  Country.create! do |country|
-#    new_row = row.to_hash
-#    puts "sssssssss", new_row.inspect
-#    
-##    country.id = row[2]
-##    country.name = row[1]
-#  end
-#   put
-#end
-City.destroy_all
-File.open(Rails.root.join('db','migrate','GeoWorldMap', 'cities.txt'), "r:ISO-8859-1").each do |line|
-  line.to_s.force_encoding("UTF-8")
-  city_from_file =  line.split(",")
-  City.create! do |city|
-  city.country_id=city_from_file[1].to_i
-  city.region_id=city_from_file[2].to_i
-  city.name=city_from_file[3].to_s
   end
 end
- 
+
+City.destroy_all
+File.open(Rails.root.join('db','migrate','GeoWorldMap', 'cities.txt'), "r:ISO-8859-1").each do |line|
+  city_from_file =  line.split(",")
+  unless (city_from_file[3].gsub /"/, '') == "City"
+    City.create! do |city|
+      city.id=city_from_file[0].to_i
+      city.country_id=city_from_file[1].to_i
+      city.region_id=city_from_file[2].to_i
+      city.name=city_from_file[3].gsub /"/, ''
+    end
+  end
+end
 
 Region.destroy_all
 File.open(Rails.root.join('db','migrate','GeoWorldMap', 'Regions.txt'), "r:ISO-8859-1").each do |line|
-  line.to_s.force_encoding("UTF-8")
   region_from_file =  line.split(",")
-  Region.create! do |region|
-    region.id=region_from_file[0].to_i
-    region.country_id=region_from_file[1].to_i
-    region.name=region_from_file[2].to_s
-    
+  unless (region_from_file[2].gsub /"/, '') == "Region"
+    Region.create! do |region|
+      region.id=region_from_file[0].to_i
+      region.country_id=region_from_file[1].to_i
+      region.name=region_from_file[2].gsub /"/, ''
+    end
   end
 end
-#CSV.foreach(Rails.root.join("cities.csv"), headers: true) do |row|
-# Country.create! do |city|
-#  city.id = row[0]
-# city.name = row[1]
-#nd
-#end
+
